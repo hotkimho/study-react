9b05cdc2a78eea455ccd81f95c787693778d8a3d
kim ho <hokim@HOui-MacBookPro.local>
2023-02-12 20:47:53 +0900

    Init react

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  brecketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  semi: false,
+  printWidth: 90
+}
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import "./App.css";
+ 
+function App() {
+  console.log("qwjioejqwioej");
+  return (
+    <div className="App">
+   qwjeoqwejoqijio
+    </div>
+  );
+}
+
+export default App;
